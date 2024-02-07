f750b428c32146c55c342bb5a92652163deefb64
lubingau <lubingau@hotmail.com>
2024-02-07 13:19:39 +0100

    Clearer script

--- sr7_vai_flow/target_zcu102/run_target.sh
+++ sr7_vai_flow/target_zcu102/run_target.sh
@@ -22,33 +22,57 @@ clean() {
 dataset() {
     echo " "
     echo "##################################################################################"
-    echo "EXTRACT DATASET"
+    echo " EXTRACT DATASET"
     echo "##################################################################################"
     echo " "
 
-    unzip ../sr7_dataset.zip -d ../
+    if [ ! -f "../sr7_dataset.zip" ]; then
+        echo "[SR7 INFO] Dataset not found"
+        echo "[SR7 INFO] Please import the dataset and place it in the /home/petalinux/ folder"
+        exit 1
+    fi
 
-    echo "[DZ INFO] Dataset extracted"
+    if [ ! -d "../sr7_dataset" ]; then
+        unzip ../sr7_dataset.zip -d ../
+        echo "[SR7 INFO] Dataset extracted"
+    else
+        echo "[SR7 INFO] Dataset already exists"
+    fi
 }
 
 # compile CNN application
 compile() {
     echo " "
     echo "##################################################################################"
-    echo "COMPILE CNN APPLICATION"
+    echo " COMPILE CNN APPLICATION"
     echo "##################################################################################"
     echo " "
-    cd code
-    bash -x ./build_app.sh
-    mv code ../run_cnn
-    # bash -x ./build_get_dpu_fps.sh
-    # mv code ../get_dpu_fps
-    cd ..
-    echo "[DZ INFO] CNN application compiled"
+
+    if [ "$1" = true ]; then
+        echo "[SR7 INFO] Compiling CNN application"
+        cd code
+        bash -x ./build_app.sh
+        mv code ../run_cnn
+        # bash -x ./build_get_dpu_fps.sh
+        # mv code ../get_dpu_fps
+        cd ..
+        echo "[SR7 INFO] CNN application compiled"
+    else
+        echo "[SR7 INFO] Skipping CNN application compilation"
+    fi
 }
 
-# now run semantic segmentation with 3 CNNs using VART C++ APIs with single thread
+# now run the CNN model
 run_models() {
+    echo " "
+    echo "##################################################################################"
+    echo " RUN CNN MODELS"
+    echo "##################################################################################"
+    echo " "
+
+    mkdir outputs 2> /dev/null
+
+    echo "[SR7 INFO] Running CNN model"
     ./run_cnn ./fsrcnn6_relu/model/fsrcnn6_relu.xmodel  ../sr7_dataset/test/blr/ 6 0 1 2> /dev/null | tee ./rpt/logfile_cpp_fsrcnn6_relu.txt
 }
 
@@ -57,18 +81,15 @@ run_fps() {
     bash -x ./code/run_cnn_fps.sh 2> /dev/null | tee ./rpt/logfile_fps.txt
 }
 
+# MAIN
+if [ "$1" == "--no-compilation" ]; then
+    compilation=false
+else
+    compilation=true
+fi
 
 #clean
-if [ ! -d "../sr7_dataset" ]; then
-    dataset
-else
-    echo "Dataset already exists"
-fi
-if [ "$1" != "--no-compile" ]; then
-    compile
-else
-    echo "Skipping compilation"
-fi
-mkdir outputs 2> /dev/null
+dataset
+compile $compilation
 run_models
 #run_fps
