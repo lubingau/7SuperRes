d4cf6440e167d864a4604f4768c23cddd2182cf1
JulienGleyze <juliengleyze3@gmail.com>
2024-02-14 11:33:31 +0100

    updated run_target.sh

--- sr7_vai_flow/target_zcu102/run_target.sh
+++ sr7_vai_flow/target_zcu102/run_target.sh
@@ -52,8 +52,8 @@ compile() {
     if [ "$1" = true ]; then
         echo "[SR7 INFO] Compiling CNN application"
         cd code
-        #bash -x ./build_app.sh
-        g++ -o build/SuperRes7 src/SuperRes7.cpp `pkg-config --cflags --libs opencv4`
+        bash -x ./build_app.sh
+        #g++ -o build/SuperRes7 src/SuperRes7.cpp `pkg-config --cflags --libs opencv4`
         g++ -o build/build_mask src/build_mask.cpp `pkg-config --cflags --libs opencv4`
         #mv code build/SuperRes7
         # bash -x ./build_get_dpu_fps.sh
