cf9f61b704374c56d075b78cd447c48d372b8780
Juliette Perrier <juliette.perrier34@gmail.com>
2024-02-27 15:36:17 +0100

    added build condition

--- sr7_vai_flow/target_zcu102/eval.sh
+++ sr7_vai_flow/target_zcu102/eval.sh
@@ -2,7 +2,18 @@
 
 # Evaluate the performance of the CNN application by measuring PSNR 
 
-mkdir build 2> /dev/null
-bash -x ./code/build_eval.sh
-mv code/build_eval build/eval
+if [ "$1" = "--build" ]; then
+    echo "[SR7 INFO] Compiling evaluation application"
+    mkdir build 2> /dev/null
+    bash -x ./code/build_eval.sh
+    echo " "
+    mv code/build_eval build/eval
+elif [ "$1" = '--no-build']; then
+    echo "[SR7 INFO] Skipping evaluation application compilation"
+else
+    echo "Usage: ./eval.sh [--build|--no-build]"
+    exit 1
+fi
+echo " "
 ./build/eval "../sr7_dataset/test/blr" "../sr7_dataset/test/gt" "/home/petalinux/target_zcu102/fsrcnn6_relu/model/fsrcnn6_relu.xmodel"
+
