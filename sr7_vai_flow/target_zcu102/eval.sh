c29405a79e2de3d6b07c134eb11d8150d037883d
Juliette Perrier <juliette.perrier34@gmail.com>
2024-02-27 11:36:39 +0100

    changed path

--- sr7_vai_flow/target_zcu102/eval.sh
+++ sr7_vai_flow/target_zcu102/eval.sh
@@ -3,6 +3,6 @@
 # Evaluate the performance of the CNN application by measuring PSNR 
 
 mkdir build 2> /dev/null
-bash -x ./build_eval.sh
+bash -x ./code/build_eval.sh
 mv code/build_eval build/eval
 ./build/eval "../sr7_dataset/test/blr" "../sr7_dataset/test/gt" "/home/petalinux/target_zcu102/fsrcnn6_relu/model/fsrcnn6_relu.xmodel"
